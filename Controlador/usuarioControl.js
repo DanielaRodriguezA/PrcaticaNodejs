0d3c25570a6bf4b83e4da9f1fddc28435bf6cc54
daniela <daniela_ra@tesch.edu.mx>
2021-11-26 15:53:16 -0600

    version 2

--- Controlador/usuarioControl.js
+++ Controlador/usuarioControl.js
@@ -1,10 +1,7 @@
-'use strict'
-
 const bcrypt = require('bcrypt');
 var usuariosModelo = require('../modelo/usuarios');
 var usuario = new usuariosModelo();
-
-const { param } = require('../rutas/usuariosRuta');;
+var jwt = require('../servicio/jwt')
 
 function prueba(req, res) {
     res.status(200).send({
@@ -13,14 +10,13 @@ function prueba(req, res) {
 }
 
 function registrarUsuario(req, res) {
-
     var params = req.body; //recibe todos los datos por Por el Metodo POST
     console.log(params);
 
     usuario.nombre = params.nombre;
     usuario.apellido = params.apellido;
     usuario.email = params.email;
-    usuario.rol = 'ROLE_USER';
+    usuario.rol = 'ROLE_ADMIN';
     usuario.imagen = 'null';
 
     if (params.password) {
@@ -33,7 +29,7 @@ function registrarUsuario(req, res) {
                         res.status(500).send({ mesagge: 'Error al guardar el usuario' });
                     } else {
                         if (!usuarioAlmacenado) {
-                            res.status(404).send({ mesagge: 'No se ha registrado el ususario' });
+                            res.status(404).send({ mesagge: 'No se ha registrado el usuario' });
                         } else {
                             //nos devuelve un objeto con los datos del ususario guardado
                             res.status(200).send({ usuarios: usuarioAlmacenado });
@@ -47,7 +43,7 @@ function registrarUsuario(req, res) {
         });
 
     } else {
-        res.status(500).send({ mesagge: 'Introduce la contraseña' });
+        res.status(404).send({ mesagge: 'Introduce la contraseña' });
     }
 
 }
@@ -59,7 +55,7 @@ function accesoUsuario(req, res) {
 
     usuariosModelo.findOne({ email: email }, (err, user) => {
         if (err) {
-            res.status(500).send({ mesagge: 'Error en la peticion' });
+            res.status(500).send({ mesagge: 'Error en la peticion al servidor' });
         } else {
             if (!user) {
                 res.status(404).send({ mesagge: 'El usuario no existe' });
@@ -69,24 +65,26 @@ function accesoUsuario(req, res) {
                         //devolver los datos del ususario logeado
                         console.log('coincide el password')
                         if (params.gethash) {
+                            res.status(200).send({
+                                token: jwt.createToken(user)
+                            });
                             //devolver un token de jwt
                         } else {
                             res.status(200).send({ user: user });
                         }
                     } else {
-                        res.status(404).send({
-                            mesagge: 'El usuario no se ha identificado '
-                        });
+                        res.status(404).send({ mesagge: 'El usuario no se ha identificado' });
                     }
                 });
             }
-
         }
     });
 }
 
+
+
 module.exports = {
     prueba,
     registrarUsuario,
-    accesoUsuario
+    accesoUsuario,
 };
